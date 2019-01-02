663c76d7f9577d1a4760b20eae53c79d8e17725c
rahul-racha <rahul_rachamalla@outlook.com>
2019-01-02 14:07:37 -0500

    Tested Percolation.java

--- algo_part1/union_find/percolation/Percolation.java
+++ algo_part1/union_find/percolation/Percolation.java
@@ -3,6 +3,8 @@
  *  Date:
  *  Description:
  **************************************************************************** */
+import edu.princeton.cs.algs4.StdRandom;
+
 import java.util.HashMap;
 
 public class Percolation {
@@ -14,23 +16,30 @@ public class Percolation {
     // API
     public Percolation(int n)                // create n-by-n grid, with all sites blocked
     {
-        this.n = n;
-        this.site = new Site[n][n];
-        this.size = new int[n][n];
-        this.countToLocMap = new HashMap<Integer, Location>();
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j =0; j < n; j++) {
-                this.site[i][j] = new Site();
-                this.site[i][j].value = count;
-                this.site[i][j].type = SiteType.blocked;
-                this.size[i][j] = 1;
-                this.site[i][j].rootLoc = new Location();
-                this.site[i][j].rootLoc.row = i;
-                this.site[i][j].rootLoc.col = j;
-                this.countToLocMap.put(count, this.site[i][j].rootLoc);
-                count++;
+        try {
+            if (n <= 0) {
+                throw new IllegalArgumentException("n must not be <= 0");
             }
+            this.n = n;
+            this.site = new Site[n][n];
+            this.size = new int[n][n];
+            this.countToLocMap = new HashMap<Integer, Location>();
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    this.site[i][j] = new Site();
+                    this.site[i][j].value = count;
+                    this.site[i][j].type = SiteType.blocked;
+                    this.size[i][j] = 1;
+                    this.site[i][j].rootLoc = new Location();
+                    this.site[i][j].rootLoc.row = i;
+                    this.site[i][j].rootLoc.col = j;
+                    this.countToLocMap.put(count, this.site[i][j].rootLoc);
+                    count++;
+                }
+            }
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception);
         }
     }
     private Site getRootFor(Site site) {
@@ -42,7 +51,7 @@ public class Percolation {
         }
         return site;
     }
-    private Boolean areLocationsEqual(Location loc1, Location loc2) {
+    private boolean areLocationsEqual(Location loc1, Location loc2) {
         if (loc1 == null || loc2 == null) {
             return false;
         }
@@ -68,59 +77,11 @@ public class Percolation {
             }
         }
     }
-
     private Location getLocWithMaxSize(Location topSiteLoc, Location leftSiteLoc, Location rightSiteLoc,
                                            Location bottomSiteLoc) {
         Location firstSetMaxSizeLoc = getMaxLocation(topSiteLoc, leftSiteLoc);
         Location secondSetMaxSizeLoc = getMaxLocation(rightSiteLoc, bottomSiteLoc);
         return getMaxLocation(firstSetMaxSizeLoc, secondSetMaxSizeLoc);
-        // //First Set Max Location
-        // if (topSiteLoc == null && leftSiteLoc == null) {
-        //     firstSetMaxSizeLoc = null;
-        // } else if (topSiteLoc != null && leftSiteLoc == null) {
-        //     firstSetMaxSizeLoc = topSiteLoc;
-        // } else if (topSiteLoc == null && leftSiteLoc != null) {
-        //     firstSetMaxSizeLoc = leftSiteLoc;
-        // } else {
-        //     if (this.size[topSiteLoc.row][topSiteLoc.col] >=
-        //             this.size[leftSiteLoc.row][leftSiteLoc.col]) {
-        //         firstSetMaxSizeLoc = topSiteLoc;
-        //     } else {
-        //         firstSetMaxSizeLoc = leftSiteLoc;
-        //     }
-        // }
-        //
-        // // Second Set Max Location
-        // if (rightSiteLoc == null && bottomSiteLoc == null) {
-        //     secondSetMaxSizeLoc = null;
-        // } else if (rightSiteLoc != null && bottomSiteLoc == null) {
-        //     secondSetMaxSizeLoc = rightSiteLoc;
-        // } else if (rightSiteLoc == null && bottomSiteLoc != null) {
-        //     secondSetMaxSizeLoc = bottomSiteLoc;
-        // } else {
-        //     if (this.size[rightSiteLoc.row][rightSiteLoc.col] >=
-        //             this.size[bottomSiteLoc.row][bottomSiteLoc.col]) {
-        //         secondSetMaxSizeLoc = rightSiteLoc;
-        //     } else {
-        //         secondSetMaxSizeLoc = bottomSiteLoc;
-        //     }
-        // }
-
-        // // Final Max Location
-        // if (firstSetMaxSizeLoc == null && secondSetMaxSizeLoc == null) {
-        //     return null;
-        // } else if (firstSetMaxSizeLoc != null && secondSetMaxSizeLoc == null) {
-        //     return firstSetMaxSizeLoc;
-        // } else if (firstSetMaxSizeLoc == null && secondSetMaxSizeLoc != null) {
-        //     return secondSetMaxSizeLoc;
-        // } else {
-        //     if (this.size[firstSetMaxSizeLoc.row][firstSetMaxSizeLoc.col] >=
-        //             this.size[secondSetMaxSizeLoc.row][secondSetMaxSizeLoc.col]) {
-        //         return firstSetMaxSizeLoc;
-        //     } else {
-        //         return secondSetMaxSizeLoc;
-        //     }
-        // }
     }
     private void updateRootLocForSites(Site maxSizeSite, Site site1, Site site2, Site site3) {
         if (maxSizeSite == null) {
@@ -162,56 +123,72 @@ public class Percolation {
             }
         }
         if (0 == track) { return; }
+
+        Site topSiteRoot = (topSite != null && topSite.type != SiteType.blocked) ?
+                           getRootFor(topSite) : null;
+        Site leftSiteRoot = (leftSite != null && leftSite.type != SiteType.blocked) ?
+                            getRootFor(leftSite) : null;
+        Site rightSiteRoot = (rightSite != null && rightSite.type != SiteType.blocked) ?
+                             getRootFor(rightSite) : null;
+        Site bottomSiteRoot = (bottomSite != null && bottomSite.type != SiteType.blocked) ?
+                              getRootFor(bottomSite) : null;
+
+
+        Location maxSizeSiteLoc = this.getLocWithMaxSize((topSiteRoot != null) ? topSiteRoot.rootLoc: null,
+                                                         (leftSiteRoot != null) ? leftSiteRoot.rootLoc: null,
+                                                         (rightSiteRoot != null) ? rightSiteRoot.rootLoc: null,
+                                                         (bottomSiteRoot != null) ? bottomSiteRoot.rootLoc: null);
+        if (maxSizeSiteLoc == null) {
+            return;
+        }
+
+        this.site[row][col].rootLoc = maxSizeSiteLoc;
+        if (topSiteRoot != null && this.areLocationsEqual(maxSizeSiteLoc, topSiteRoot.rootLoc)) {
+            this.updateRootLocForSites(topSiteRoot, leftSiteRoot, rightSiteRoot,
+                                       bottomSiteRoot);
+        }
+        else if (leftSiteRoot != null && this.areLocationsEqual(maxSizeSiteLoc, leftSiteRoot.rootLoc)) {
+            this.updateRootLocForSites(leftSiteRoot, topSiteRoot, rightSiteRoot,
+                                       bottomSiteRoot);
+        }
+        else if (rightSiteRoot != null && this.areLocationsEqual(maxSizeSiteLoc, rightSiteRoot.rootLoc)) {
+            this.updateRootLocForSites(rightSiteRoot, topSiteRoot, leftSiteRoot,
+                                       bottomSiteRoot);
+        }
+        else {
+            this.updateRootLocForSites(bottomSiteRoot, topSiteRoot, leftSiteRoot,
+                                       rightSiteRoot);
+        }
+
         track = 0;
         for (Site adjacentSite: adjacentSites) {
             if (adjacentSite != null && adjacentSite.type == SiteType.full) {
                 track += 1;
             }
         }
-        // current site is open & all adjacent site are either blocked or open but not full
-        if (track == 0 && this.site[row][col].type == SiteType.open) {
-            Site topSiteRoot = (topSite != null) ? getRootFor(topSite) : null;
-            Site leftSiteRoot = (leftSite != null) ? getRootFor(leftSite) : null;
-            Site rightSiteRoot = (rightSite != null) ? getRootFor(rightSite) : null;
-            Site bottomSiteRoot = (bottomSite != null) ? getRootFor(bottomSite) : null;
 
-            Location maxSizeSiteLoc = this.getLocWithMaxSize(topSiteRoot.rootLoc,
-                                                                 leftSite.rootLoc,
-                                                                 rightSite.rootLoc,
-                                                                 bottomSite.rootLoc);
-            if (maxSizeSiteLoc != null) {
-                this.site[row][col].rootLoc = maxSizeSiteLoc;
-                if (this.areLocationsEqual(maxSizeSiteLoc, topSiteRoot.rootLoc)) {
-                    this.updateRootLocForSites(topSiteRoot, leftSite, rightSite, bottomSite);
-                }
-                else if (this.areLocationsEqual(maxSizeSiteLoc, leftSite.rootLoc)) {
-                    this.updateRootLocForSites(leftSite, topSite, rightSite, bottomSite);
-                }
-                else if (this.areLocationsEqual(maxSizeSiteLoc, rightSite.rootLoc)) {
-                    this.updateRootLocForSites(rightSite, topSite, leftSite, bottomSite);
-                }
-                else {
-                    this.updateRootLocForSites(bottomSite, topSite, leftSite, rightSite);
-                }
-            } else {
-                return;
-            }
-        } // current site is full & all adjacent site are either blocked or open but not full
-        else if (track == 0 && this.site[row][col].type == SiteType.full) {
-            
+        // current site is full or atleast one of the adjacent sites are full
+        if (track > 0 || this.site[row][col].type == SiteType.full) {
+            this.site[maxSizeSiteLoc.row][maxSizeSiteLoc.col].type = SiteType.full;
+        } else {
+            // current site is open & all adjacent site are either blocked or open but not full
+            return;
         }
-
     }
     public boolean isOpen(int row, int col)  // is site (row, col) open?
     {
-        if (SiteType.blocked == this.site[row][col].type) {
-            return false;
+        // SiteType currentSiteType = this.getRootFor(this.site[row][col]).type;
+        // if (SiteType.full == currentSiteType || SiteType.open == currentSiteType) {
+        //     return true;
+        // }
+        if (SiteType.blocked != this.site[row][col].type) {
+            return true;
         }
-        return true;
+        return false;
     }
     public boolean isFull(int row, int col)  // is site (row, col) full?
     {
-        if (SiteType.full == this.site[row][col].type) {
+        if (SiteType.full == this.getRootFor(this.site[row][col]).type) {
             return true;
         }
         return false;
@@ -223,20 +200,38 @@ public class Percolation {
     public boolean percolates()              // does the system percolate?
     {
         for (int i = 0; i < this.n; i++) {
-            if (SiteType.full == this.site[this.n-1][i].type) {
+            // if (SiteType.full == this.site[this.n-1][i].type) {
+            //     return true;
+            // }
+            if (SiteType.full == this.getRootFor(this.site[this.n-1][i]).type) {
                 return true;
             }
         }
         return false;
     }
     public Site[][] getGrid() {
-        return this.site;
+        return this.site.clone();
     }
-
+    public double getThreshold() {
+        return (double)(this.noOfOpenSites)/(this.n*this.n);
+    }
     public static void main(String[] args) {
-        Percolation grid = new Percolation(20);
-        Site site[][] = grid.getGrid();
-        System.out.println("Size: "+ site.length);
+        int number = 5;
+        Percolation grid = new Percolation(number);
+        // Site site[][] = grid.getGrid();
+        // System.out.println("Size: "+ site.length);
+        int row = 0, col = 0;
+        while(true) {
+            row = StdRandom.uniform(0, number);
+            col = StdRandom.uniform(0, number);
+            System.out.println("("+row+","+col+")");
+            grid.open(row, col);
+            if (grid.percolates()) {
+                System.out.println("The system percolates!");
+                System.out.println("The threshold value is:" + grid.getThreshold());
+                break;
+            }
+        }
     }
 }
 
---
+++ algo_part1/union_find/percolation/PercolationStats.java
@@ -0,0 +1,72 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+    double[] trialsCount;
+    double confidenceLevel = 1.96;
+    public PercolationStats(int n, int trials)    // perform trials independent experiments on an n-by-n grid
+    {
+        try {
+            if (trials <= 0) {
+                throw new IllegalArgumentException("trials must not be <= 0");
+            }
+            for (int i = 0; i < trials; i++) {
+                Percolation grid = new Percolation(n);
+                int row = 0, col = 0;
+                while(true) {
+                    row = StdRandom.uniform(0, n);
+                    col = StdRandom.uniform(0, n);
+                    // System.out.println("("+row+","+col+")");
+                    grid.open(row, col);
+                    if (grid.percolates()) {
+                        // System.out.println("The system percolates!");
+                        System.out.println("The threshold value for "+ "case "+i+" is:" + grid.getThreshold());
+                        grid = null;
+                        break;
+                    }
+                }
+            }
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception);
+        }
+    }
+    public double mean()                          // sample mean of percolation threshold
+    {
+        return StdStats.mean(trialsCount);
+    }
+    public double stddev()                        // sample standard deviation of percolation threshold
+    {
+        return StdStats.stddev(trialsCount);
+    }
+    public double confidenceLo()                  // low  endpoint of 95% confidence interval
+    {
+        double temp = confidenceLevel * stddev() / Math.sqrt(trialsCount.length);
+        return mean() - temp;
+    }
+    public double confidenceHi()                  // high endpoint of 95% confidence interval
+    {
+        double temp = confidenceLevel * stddev() / Math.sqrt(trialsCount.length);
+        return mean() + temp;
+    }
+
+    public static void main(String[] args) {
+        if (args.length == 2) {
+            System.out.println(args[0]);
+            System.out.println(args[1]);
+            int n = Integer.parseInt(args[0]);
+            int trials = Integer.parseInt(args[1]);
+            PercolationStats stats = new PercolationStats(n, trials);
+            System.out.println("mean    =" + stats.mean());
+            System.out.println("stddev  =" + stats.stddev());
+            String confidenceInterval = "[" + stats.confidenceLo() + ", " + stats.confidenceHi() +
+                    "]";
+            System.out.println("95% confidence interval = " + confidenceInterval);
+        }
+    }
+}
